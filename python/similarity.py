b00e1af37f20b24a16040941a025e4d785a11097
Lucas May Petry <lucasmaypetry@gmail.com>
2018-11-13 09:35:59 -0200

    Implemented EDR, LCSS, and MSM

--- python/similarity.py
+++ python/similarity.py
@@ -3,15 +3,66 @@ import numpy as np
 
 
 def msm(t1, t2, dist_functions, thresholds, features=None, weights=None):
-    pass
+    def score(p1, p2):
+        matches = np.zeros(len(p1))
+        for i, _ in enumerate(p1):
+            matches[i] = dist_functions[i](p1[i], p2[i]) <= thresholds[i]
+        return np.sum(matches * weights)
+
+    matrix = np.zeros(shape=(len(t1), len(t2)))
+    compute_score = np.vectorize(score)
+
+    for i, p1 in enumerate(t1):
+        matrix[i] = compute_score(p1, t2)
+
+    parity1 = np.sum(np.amax(matrix, axis=1))
+    parity2 = np.sum(np.amax(np.transpose(matrix), axis=1))
+    return (parity1 + parity2) / (len(t1) + len(t2))
 
 
 def lcss(t1, t2, dist_functions, thresholds, features=None):
-    pass
+    def match(p1, p2):
+        for i, _ in enumerate(p1):
+            d = dist_functions[i](p1[i], p2[i])
+            if d > thresholds[i]:
+                break
+        else:
+            return True
+        return False
+
+    matrix = np.zeros(shape=(len(t1) + 1, len(t2) + 1))
+
+    for i, p1 in enumerate(t1):
+        for j, p2 in enumerate(t2):
+            if match(p1, p2):
+                matrix[i+1][j+1] = matrix[i][j] + 1
+            else:
+                matrix[i+1][j+1] = max(matrix[i+1][j], matrix[i][j+1])
+
+    return matrix[len(t1)][len(t2)] / min(len(t1), len(t2))
 
 
 def edr(t1, t2, dist_functions, thresholds, features=None):
-    pass
+    def match_cost(p1, p2):
+        for i, _ in enumerate(p1):
+            d = dist_functions[i](p1[i], p2[i])
+            if d > thresholds[i]:
+                break
+        else:
+            return 0
+        return 1
+
+    matrix = np.zeros(shape=(len(t1) + 1, len(t2) + 1))
+    matrix[:, 0] = np.r_[0:len(t1)+1]
+    matrix[0] = np.r_[0:len(t2)+1]
+
+    for i, p1 in enumerate(t1):
+        for j, p2 in enumerate(t2):
+            cost = match_cost(p1, p2)
+            matrix[i+1][j+1] = min(matrix[i][j] + cost,
+                                   min(matrix[i+1][j] + 1, matrix[i][j+1] + 1))
+
+    return 1 - matrix[len(t1)][len(t2)] / max(len(t1), len(t2))
 
 
 def pairwise_similarity(X, Y=None, measure=msm, n_jobs=1, **kwargs):
@@ -19,7 +70,7 @@ def pairwise_similarity(X, Y=None, measure=msm, n_jobs=1, **kwargs):
 
     Parameters
     ----------
-    X : ndarray, shape: (n_trajectories_X, n_features)
+    X : ndarray, shape: (n_trajectories_X, n_points, n_features)
         Input data.
     Y : ndarray, shape: (n_trajectories_Y, n_features)
         Input data. If ``None``, the output will be the pairwise
@@ -34,7 +85,7 @@ def pairwise_similarity(X, Y=None, measure=msm, n_jobs=1, **kwargs):
     Returns
     -------
     similarities : array
-        An array with shape (n_trajectories_X, n_trajectories_Y).
+        An array with shape (n_trajectories_X, n_points, n_trajectories_Y).
     """
     Y = X if not Y else Y
     similarity = np.zeros(shape=(len(X), len(Y)))
